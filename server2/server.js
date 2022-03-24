53fe6743a3cef61f1f0fe4b00f0f5462ccc2c1ca
Rafiul29 <rafiulislam4472@gmail.com>
2022-03-24 18:03:29 +0600

    communication server1  and server2

--- server2/server.js
+++ server2/server.js
@@ -1,7 +1,36 @@
 
 const express = require('express')
+var http =require('http')
+const cors = require('cors');
+const axios = require('axios');
+const redis =require('redis');
+const { createClient } = require('redis');
+var querystring = require('querystring');
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3002
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/server2',(req,res)=>{
+
+  let sum = req.body.a+req.body.b 
+  (async ()=>{
+    const client=createClient({
+      url: "redis://docker-composee-redis-1:6379"
+    });
+    client.on('error',(err)=> console.log("Redis Client Error",err));
+    await client.connect();
+    await client.set('result',sum);
+    const value = await client.get('result')
+    console.log("reached here 1")
+    res.json({msg:'welcome you all',resultvalue});
+  })
+
+
+})
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World! 02 ')
